217de3e9ec95710b25db6c9b3d755c2d71b3c2de
Ogabek Erkinov <ogabekerkinov56@gmail.com>
2025-08-07 19:47:03 +0500

    keraksiz qism olib tashlandi

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const BASE_URL = "https://api.weatherapi.com/v1"
-const api_key = import.meta.env.VITE_API_KEY
 
 const privateApi = axios.create({
   baseURL: BASE_URL, 
