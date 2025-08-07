2c8cf46078a065639d5a3846f93c6157be7ebf85
Ogabek Erkinov <ogabekerkinov56@gmail.com>
2025-08-07 08:00:13 +0500

    search panel has been updated

--- src/widgets/SearchPanel.jsx
+++ src/widgets/SearchPanel.jsx
@@ -3,18 +3,23 @@ import { CiSearch } from "react-icons/ci";
 
 const SearchPanel = () => {
   return (
-    <div className="h-[50px] w-[200px]flex items-center gap-2 px-3 border border-gray-300 rounded-full bg-white shadow-sm">
-      <input
-        type="text"
-        placeholder="Qidirish..."
-        className="flex-grow h-full bg-transparent outline-none text-md font-bold text-gray-700"
-      />
-      <motion.button
-        whileTap={{ scale: 0.85 }}
-        className="text-gray-600 hover:text-blue-500 transition-colors duration-200"
-      >
-        <CiSearch size={24} />
-      </motion.button>
+    <div className="h-[50px] w-[80%] sm:w-[300px] border border-gray-300 
+                    rounded-full bg-white shadow-sm">
+        <div className="h-full w-full flex justify-center items-center p-2">
+
+          {/* input to insert country name */}
+           <input  type="text"
+                   placeholder="Qidirish..."
+                   className="w-[90%] flex-grow h-full bg-transparent outline-none
+                              text-md font-bold text-gray-700" />
+          
+          {/* search button */}
+           <motion.button
+              whileTap={{ scale: 0.85 }}
+              className="text-gray-600 hover:text-blue-500 transition-colors duration-200 cursor-pointer">
+              <CiSearch size={24} />
+           </motion.button>
+        </div>
     </div>
   );
 };
