6516c33e1ab395834e823f9402a40aa008659f8b
Ogabek Erkinov <ogabekerkinov56@gmail.com>
2025-08-08 05:35:27 +0500

    alert state bugs is fixed

--- src/states/alertState.js
+++ src/states/alertState.js
@@ -2,13 +2,12 @@ import { create } from "zustand";
 
 
 const useAlert = create((set) => ({
-  isAlert: true,
+  isAlert: false,
   setIsAlert: (payload) => set({ isAlert: payload }),
 
   message: '',
   setMessage: (payload) => set({ message: payload }),
 
-  404 : false
 }));
 
 export default useAlert
